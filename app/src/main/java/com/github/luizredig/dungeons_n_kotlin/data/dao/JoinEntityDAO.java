b77b375dceba67f75909cc83a69beaadf9328766
redig <luizredig@gmail.com>
2024-09-30 20:08:01 -0300

    chore(database): prepare project for SQLite integration

---
+++ app/src/main/java/com/github/luizredig/dungeons_n_kotlin/data/dao/JoinEntityDAO.java
@@ -0,0 +1,4 @@
+package com.github.luizredig.dungeons_n_kotlin.data.dao;
+
+public class JoinEntityDAO {
+}
---
+++ app/src/main/java/com/github/luizredig/dungeons_n_kotlin/data/entities/JoinEntity.java
@@ -0,0 +1,4 @@
+package com.github.luizredig.dungeons_n_kotlin.data.entities;
+
+public class JoinEntity {
+}
